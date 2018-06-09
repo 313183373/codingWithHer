f90a9cded8e28206a9d6069ca4910f83bf15c3cd
313183373 <313183373@qq.com>
2018-06-09 11:33:36 +0800

    add book test

--- spec/bookingSpec.js
+++ spec/bookingSpec.js
@@ -1,6 +1,6 @@
 const TennisManager = require('../TennisManager');
 
-describe("BookingTest", function() {
+describe("TestInput", function() {
     it("should return error when input is wrong", function() {
         // given
         let input = 'abd14';
@@ -21,4 +21,30 @@ describe("BookingTest", function() {
         // then
         expect(decodeResult).toBe(true);
     });
+});
+
+describe("TestBooking", function() {
+    
+    // fail
+    it('should return false when time is not the whole point', function () {
+
+    });
+
+    it('should return false when time is not during the correct time', function () {
+
+    });
+
+    it('should return false when the court is being booked', function () {
+
+    });
+    
+    // success
+    it('should return true when the time is cross the time', function () {
+
+    });
+
+    it('should return true when the time is not cross the time', function () {
+
+    });
+
 });
